e15911cb9623062072b4717a86c3497ba2e7370f
Marco Cortes <marcocortes95@gmail.com>
2019-01-31 20:29:30 -0600

    first commit

---
+++ 2048.py
@@ -0,0 +1,18 @@
+import pyxel
+import math
+
+class App:
+    def __init__(self):
+        pyxel.init(250,250)
+        pyxel.mouse(True)
+
+        pyxel.run(self.update,self.draw)
+
+    def update(self):
+        if pyxel.btnp(pyxel.KEY_Q):
+            pyxel.quit()
+
+    def draw(self):
+        pyxel.cls(0)
+
+App()
